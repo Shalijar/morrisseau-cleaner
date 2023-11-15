26ffb3add3a5462a7fbb6cf3e1e582cba270fd87
Shalijar <ali0shajari@gmail.com>
2023-11-15 15:50:43 -0500

    Added functions and terminal file

--- morrisseau_cleaner/morrisseau_cleaner.py
+++ morrisseau_cleaner/morrisseau_cleaner.py
@@ -1,2 +1,12 @@
-def check() -> str:
-    return "this is a check"
+import terminal as t
+
+
+### Global variables ###
+log_file = 'change_log.txt'
+input_file = 'input.csv'
+
+def main():
+    t.terminal()
+
+if __name__ == "__main__":
+    main()
