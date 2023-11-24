a4ae4174c24646c9edc2c7bf2afcf4b00a116ce2
Shalijar <ali0shajari@gmail.com>
2023-11-23 20:14:16 -0500

    Added functions to terminal

--- morrisseau_cleaner/terminal.py
+++ morrisseau_cleaner/terminal.py
@@ -9,6 +9,9 @@ def display_menu():
     print("  clean spaces - Clean spaces in the chosen file")
     print("  clean pipes - Clean pipes in the chosen file")
     print("  help - Show available commands")
+    print("  clean dates - Clean dates in the chosen file")
+    print("  clean titles - Clean titles in the chosen file")
+    print("  clean pages - Clean pages in the chosen file")
     print("  exit - Exit the program")
 
 def terminal_main():
@@ -24,19 +27,22 @@ def terminal_main():
         if command == "choose a file":
             input_file = functions.get_file_path()
             output_file = functions.get_output_file_path(input_file)
-        elif command == "clean spaces":
-            if input_file and output_file:
+        if input_file and output_file:
+            if command == "clean spaces":
                 functions.clean_spaces(input_file, output_file)
-            else:
-                print("Please choose a file first.")
-        elif command == "clean pipes":
-            if input_file and output_file:
+            elif command == "clean pipes":
                 functions.clean_pipes(input_file, output_file)
-            else:
-                print("Please choose a file first.")
-        elif command == "help":
+            elif command == "clean dates":
+                functions.clean_dates(input_file, output_file)
+            elif command == "clean titles":
+                functions.clean_titles(input_file, output_file)
+            elif command == "clean pages":
+                functions.clean_pages(input_file, output_file)
+        else: 
+            print("Please choose a file first.")
+        if command == "help":
             display_menu()
-        elif command == "exit":
+        if command == "exit":
             break
         else:
             print("Invalid command. Please try again.")
